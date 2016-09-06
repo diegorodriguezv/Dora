946a4fc72a38b0e29178fcacfc6fbbeaa5520af3
Diego Rodríguez <diegorodriguezv@gmail.com>
2016-09-06 05:01:16 -0500

    Add TUI

---
+++ dora/dora.py
@@ -0,0 +1,42 @@
+import traceback
+import RPi.GPIO as GPIO
+import motor.relaymotor
+
+
+class Dora(object):
+    pin = 7
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, False)
+        self.motor = motor.relaymotor.RelayMotor(self.motor_on, self.motor_off)
+
+
+    def motor_on(self):
+        GPIO.output(self.pin, True)
+        pass
+
+    def motor_off(self):
+        GPIO.output(self.pin, False)
+        pass
+
+    def tui_thread(self):
+        increment = 20
+        try:
+            while 1:
+                inp = raw_input().strip().upper()
+                if inp == "Q":
+                    self.motor.set_throttle(self.motor.throttle + increment)
+                elif inp == "A":
+                    self.motor.set_throttle(self.motor.throttle - increment)
+                elif inp == "Z":
+                    self.motor.set_throttle(0)
+                print "per: {} thr: {}".format(self.motor.period, self.motor.throttle)
+        except Exception as exc:
+            print "Error: in tui_thread - {0}".format(exc)
+            traceback.print_exc()
+
+
+if __name__ == "__main__":
+    Dora().tui_thread()
--- dora/motor/relaymotor.py
+++ dora/motor/relaymotor.py
@@ -1,15 +1,43 @@
+import thread
+import time
+import traceback
+
 
 class RelayMotor(object):
-    def __init__(self, on_func, off_func):
-        self.onFunc = on_func
-        self.offFunc = off_func
-        self.period = 0.1
-        self.throttle = 0
-        self.active = 0
+
+    def __init__(self, on_func, off_func, period=0.1):
+        self.on_func = on_func
+        self.off_func = off_func
+        self.period = float(period)
+        self.throttle = 0.0
+        try:
+            thread.start_new_thread(self.control_thread, ())
+            # pass
+        except Exception as exc:
+            print "Error: unable to start thread - {0}".format(exc)
+
+    def control_thread(self):
+        try:
+            while 1:
+                if self.throttle == 0:
+                    time.sleep(self.period)
+                else:
+                    self.on_func()
+                    active_period = self.throttle * self.period
+                    time.sleep(active_period)
+                    self.off_func()
+                    time.sleep(self.period - active_period)
+        except Exception as exc:
+            print "Error: in control_thread - {0}".format(exc)
+            traceback.print_exc()
 
     def set_throttle(self, throttle):
+        if throttle < 0 or throttle > 100:
+            print "Error: Invalid throttle value"
+            return
         self.throttle = throttle
-        self.active = self.period * self.throttle
+
+
 
 
 
