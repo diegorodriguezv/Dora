a1b21d06792ce3118cca271b7aa45773b93c9b55
Diego Rodriguez <diegorodriguezv@gmail.com>
2016-09-20 10:20:15 -0500

    Remove throttle factor

--- dora/dora.py
+++ dora/dora.py
@@ -153,10 +153,10 @@ class Dora(object):
 
 def convert_steering_to_2motors(throttle, steering):
     if throttle > 0:
-        factor = .2
+        factor = .4
     else:
-        factor = -.2
-    difference = factor * steering + factor * throttle
+        factor = -.4
+    difference = factor * steering
     left, right = throttle - difference, throttle + difference
     # Detect when steering surpasses max throttle, compensate reducing the opposing motor (same with min)
     if left > 1:
