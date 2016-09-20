7f8a21e8f8f6d795bb559e63d5db0c2a2f4febce
Diego Rodriguez <diegorodriguezv@gmail.com>
2016-09-20 10:09:57 -0500

    Add throttle factor to steering

--- dora/dora.py
+++ dora/dora.py
@@ -153,10 +153,10 @@ class Dora(object):
 
 def convert_steering_to_2motors(throttle, steering):
     if throttle > 0:
-        factor = .4
+        factor = .2
     else:
-        factor = -.4
-    difference = factor * steering
+        factor = -.2
+    difference = factor * steering + factor * throttle
     left, right = throttle - difference, throttle + difference
     # Detect when steering surpasses max throttle, compensate reducing the opposing motor (same with min)
     if left > 1:
