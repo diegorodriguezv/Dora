b3395ba706eb208c65935a9b8eebd9d4b3c9e23e
Diego Rodriguez <diegorodriguezv@gmail.com>
2016-09-20 10:42:11 -0500

    Test throttle in factor

--- dora/dora.py
+++ dora/dora.py
@@ -153,9 +153,9 @@ class Dora(object):
 
 def convert_steering_to_2motors(throttle, steering):
     if throttle > 0:
-        factor = .4
+        factor = .4 * throttle
     else:
-        factor = -.4
+        factor = .4 * throttle
     difference = factor * steering
     left, right = throttle - difference, throttle + difference
     # Detect when steering surpasses max throttle, compensate reducing the opposing motor (same with min)
