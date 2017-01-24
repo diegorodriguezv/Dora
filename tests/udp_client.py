be634deef672b745dc8fb0c28d6dc9e676046ee2
Diego F. Rodríguez V <diegorodriguezv@gmail.com>
2017-01-24 17:21:58 -0500

    Add test server and client for udp connections

---
+++ tests/udp_client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+# HOST, PORT = "localhost", 9999
+HOST, PORT = sys.argv[1], int(sys.argv[2])
+print sys.argv[0]
+
+data = " ".join(sys.argv[3:])
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+sock.sendto(data + "\n", (HOST, PORT))
+received = sock.recv(1024)
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
